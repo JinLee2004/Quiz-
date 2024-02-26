acb0165046088c2530065f3ed74552b7db5457a8
Jin Lee <jin.g.lee@student.curtin.edu.au>
2024-02-26 14:17:24 +0800

    Added lines and spacing to make the quiz game look nicer

--- quiz.c
+++ quiz.c
@@ -6,20 +6,20 @@
 int main(int argc, char* argv[]) {
     int total, userInput; 
 
-    printf("Welcome to a quiz game!!!");
+    printf("Welcome to a quiz game!!! \n");
     do {
-        printf("> Press 0 to start. ");
-        printf("> Press 1 to end. ");
+        printf("> Press 0 to start \n");
+        printf("> Press 1 to end \n");
         scanf("%d", &userInput);
         userInput = checkInput(userInput); /* Does not check for string input */
     }while (userInput > 1 || userInput < 0);
 
     if (userInput == 0) {
-        printf("Goodbye!"); /* User decides not to play quiz game */
+        printf("Goodbye! \n"); /* User decides not to play quiz game */
     }
     else if(userInput == 1) {
         total = quizQuestions();
+        printf("Your final score is: %d/5\n", total);
     }
-    printf("Your total score is: %d\n", total);
     return 0;
 }
